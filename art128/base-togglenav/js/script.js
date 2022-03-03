30c035f8a585e6cb98f04b806ae321e15f5705bb
mtanaka8 <mtanaka8@hawaii.edu>
2022-03-02 18:32:13 -1000

    update

--- art128/base-togglenav/js/script.js
+++ art128/base-togglenav/js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
  //TOGGLE NAV MOBILE MENU FOR SMALL SCREENS
  const menubutton = document.querySelector('.menu-button');
  const menunav = document.querySelector('.toggle-nav');
-menubutton.addEventListener('click', function(){
+menubutton.addEventListener('click',function(){
     console.log("hello!");
  });
 
