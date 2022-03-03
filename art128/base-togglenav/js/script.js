e36090c6c9903a4ceaed5a4e17d65d822be43a5a
mtanaka8 <mtanaka8@hawaii.edu>
2022-03-02 17:05:58 -1000

    update

--- art128/base-togglenav/js/script.js
+++ art128/base-togglenav/js/script.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
  const menubutton = document.querySelector('.menu-button');
 const menunav = document.querySelector('.toggle-nav');
 menubutton.addEventListener('click', function(){
-     if (menunav.getAttribute('data-navstate') === 'open') {
-         menunav.setAttribute('data-navstate', 'closed');
-
-     } else {
-     menunav.setAttribute('data-navstate', 'open');
-
-     }
+    if (menunav.getAttribute('data-navstate') === 'open') {
+        // if true do this
+        menunav.setAttribute('data-navstate', 'closed');
+    } else {
+        //else (if false) do this:
+        menunav.setAttribute('data-navstate', 'open');
+    };
      
  });
 
